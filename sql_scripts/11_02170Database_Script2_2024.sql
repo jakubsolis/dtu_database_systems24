743880112cd92f2a9145708fb807dd28d945af03
Julia <149323811+juliamiro6@users.noreply.github.com>
2024-04-04 15:51:28 +0200

    DEFINITE 11_02170Database_Script2_2024.sql

--- sql_scripts/11_02170Database_Script2_2024.sql
+++ sql_scripts/11_02170Database_Script2_2024.sql
@@ -1,6 +1,6 @@
--- PART 6: SQL DATA QUERIES
+-- PART 6: SQL Data Queries
 
--- Query 1: Find the patient’s information (patient ID, first and last name) and their corresponding
+-- QUERY 1: Find the patient’s information (patient ID, first and last name) and their corresponding
 -- doctor (doctor ID) for patients who have been prescribed Antibiotic-Type medications.
 SELECT Patient.Patient_ID, Patient.First_Name, Patient.Last_Name, 
 Appointment.Doctor_ID, Medicine.Dosage
@@ -10,7 +10,7 @@ NATURAL JOIN Medicine
 NATURAL JOIN Appointment
 WHERE Medicine.Type = 'Antibiotic';
 
--- Query 2: Find the number of rooms and the total capacity of each department
+-- QUERY 2: Find the number of rooms and the total capacity of each department
 SELECT Department.Department_Name, 
 COUNT(*) AS Department_Rooms, SUM(Room.Capacity) AS Department_Total_Capacity
 FROM Department
@@ -19,7 +19,7 @@ NATURAL JOIN Appointment
 NATURAL JOIN Room 
 GROUP BY Department.Department_Name;
 
--- Query 3: Find the contact information for all patients visited by doctor 'Daniel Harris'
+-- QUERY 3: Find the contact information for all patients visited by doctor 'Daniel Harris'
 SELECT Patient.First_Name, Patient.Last_Name, Patient.phone_number
 FROM Patient
 WHERE Patient_ID IN (
@@ -28,10 +28,11 @@ WHERE Patient_ID IN (
     JOIN Doctor ON Appointment.Doctor_ID = Doctor.Doctor_ID
     WHERE Doctor.First_Name = 'Daniel' AND Doctor.Last_Name = 'Harris');
 
+--------------------------------------------------------------------------------------------
 
 -- PART 7: SQL PROGRAMMING
 
--- Function: Create a function that calculates the average age of patients in a specific department.
+-- FUNCTION: Create a function that calculates the average age of patients in a specific department.
 DELIMITER //
 CREATE FUNCTION GetAverageAgeInDepartment(Department_ID INT)
 RETURNS DECIMAL(5, 2)
@@ -44,12 +45,13 @@ BEGIN
 END //
 DELIMITER ;
 
+-- Query
 SELECT GetAverageAgeInDepartment(8);
 SELECT Department_ID, Department_Name, GetAverageAgeInDepartment(Department_ID) AS Average_Age
 FROM Department;
 
 
--- Procedure: Schedule appointment for a patient with a specific doctor, considering the doctor's
+-- PROCEDURE: Schedule appointment for a patient with a specific doctor, considering the doctor's
 -- availability and specialty
 DELIMITER //
 CREATE PROCEDURE ScheduleAppointment(
@@ -58,10 +60,8 @@ CREATE PROCEDURE ScheduleAppointment(
     IN Scheduled_Room_ID INT,
     IN Scheduled_Day DATE,
     IN Scheduled_Start_Time TIME,
-    IN Sheduled_End_Time TIME
-)
+    IN Sheduled_End_Time TIME)
 BEGIN
-    -- Check if the doctor is available
     DECLARE Doctor_Available INT DEFAULT 0;
     
     SELECT COUNT(*)
@@ -71,7 +71,6 @@ BEGIN
     AND Day = Scheduled_Day
     AND ((Start_Time < Sheduled_End_Time AND End_Time > Scheduled_Start_Time));
 
-    -- Proceed only if the doctor is available
     IF doctor_available = 0 THEN
         INSERT INTO Appointment (Day, Start_Time, End_Time, Doctor_ID, Patient_ID, Room_ID)
         VALUES (Scheduled_Day, Scheduled_Start_Time, Sheduled_End_Time, Scheduled_Doctor_ID, 
@@ -83,14 +82,34 @@ BEGIN
 END //
 DELIMITER ;
 
+-- Queries
 SELECT*FROM Appointment;
-SELECT*FROM Doctor;
-CALL ScheduleAppointment(1, 4, 2, '2024-04-01', '09:00:00', '09:30:00');
+CALL ScheduleAppointment(1, 2, 1, '2024-04-04', '09:00:00', '09:30:00');
+SELECT*FROM Appointment;
 
+-- TRIGGER: If an appointment is scheduled for a new patient, the trigger creates a new row in Patient
+-- with the new ID provided.
+DELIMITER //
+CREATE TRIGGER CheckForNewPatient 
+BEFORE INSERT ON Appointment
+FOR EACH ROW
+BEGIN
+  DECLARE patient_exists INT;
+  SELECT COUNT(*) INTO patient_exists FROM Patient WHERE Patient_ID = NEW.Patient_ID;
 
--- Trigger: Send confirmation information to the patient about the appointment
+  IF patient_exists = 0 THEN
+    INSERT INTO Patient (Patient_ID, First_Name, Last_Name, phone_number) 
+    VALUES (NEW.Patient_ID, 'Add first name', 'Add last name', 'Add phone number');
+  END IF;
+END //
+DELIMITER ;
 
+-- Queries
+SELECT*FROM Appointment;
+CALL ScheduleAppointment(21, 19, 2, '2024-04-07', '10:00:00', '10:30:00');
+SELECT*FROM Patient;
 
+--------------------------------------------------------------------------------------------
 
 -- PART 8: SQL TABLE MODIFICATIONS
 UPDATE room
@@ -103,4 +122,4 @@ SET capacity =
 WHERE capacity > 0;
 
 DELETE FROM appointment
-WHERE DATE(Day) = ’2024-04-02’;
+WHERE DATE(Day) = '2024-04-02';
